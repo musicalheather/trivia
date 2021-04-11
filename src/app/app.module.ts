b6aa68de4b13b67583f231dabcf9a58e443ae63c
Heather Bowes <musicalheather@gmail.com>
2021-04-10 21:27:28 -0400

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route } from '@angular/router';
+import { RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TriviaComponent } from './trivia/trivia.component';
 
+const routes: Route[] = [
+  { path: 'trivia', component: TriviaComponent },
+  { path: '', component: TriviaComponent },
+  { path: '**', component: redirectTo: ''}
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    TriviaComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, TriviaComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
