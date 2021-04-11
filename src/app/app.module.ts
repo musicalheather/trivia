2a9194cecfe3f283c430de3c8865c0f806a4284b
Heather Bowes <musicalheather@gmail.com>
2021-04-10 20:54:43 -0400

    new app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TriviaComponent } from './trivia/trivia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TriviaComponent
   ],
   imports: [
     BrowserModule,
